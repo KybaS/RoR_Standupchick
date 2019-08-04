b34da8c9d453b4ca4d465b5e24b3f7acddea20a5
kybas <sergeykiba2012@gmail.com>
2019-08-04 23:35:07 +0300

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-User.create!(name:  "admin",
-             email: "admin@standupchick.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  'admin',
+             email: 'admin@standupchick.com',
+             password:              'foobar',
+             password_confirmation: 'foobar',
              admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@standupchick.org"
-  password = "password"
+  password = 'password'
   User.create!(name:  name,
                email: email,
                password:              password,
